cb19f051860e6c4ff6be3f4c50b093bce28506c0
Laurens Rinner <laurens.rinner@bulme.at>
2021-09-30 13:00:03 +0200

    interactive function added

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,15 @@
 // Hello Proramm in c++
 #include <iostream>
+#include <string.h>
+
+using namespace std;
 
 int main()
 {
+	string name;
 	std::cout <<"Hallo liebe Leute"<< std::endl;
+	cout <<"Name: ";
+	cin>>name;
+	cout<<"Hallo: "<< name << endl; 
 	return 0;
 }
